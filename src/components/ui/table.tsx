2f66bab8dfa486c598f6581e426b1325dc903a09
rakhathegreat <rakhafadhillah516@gmail.com>
2026-06-18 10:41:50 +0700

    Beta

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -86,7 +86,7 @@ const TableHead = React.forwardRef<
     ref={ref}
     data-slot="table-head"
     className={cn(
-      "h-10 px-2 text-left align-middle font-medium whitespace-nowrap text-foreground [&:has([role=checkbox])]:pr-0",
+      "h-10 px-2 text-left align-middle font-medium whitespace-nowrap text-foreground has-[[role=checkbox]]:pr-0",
       className
     )}
     {...props}
@@ -102,7 +102,7 @@ const TableCell = React.forwardRef<
     ref={ref}
     data-slot="table-cell"
     className={cn(
-      "p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0",
+      "p-2 align-middle whitespace-nowrap has-[[role=checkbox]]:pr-0",
       className
     )}
     {...props}
